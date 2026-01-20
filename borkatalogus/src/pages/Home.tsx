a4f310b6369ad72055643d26a4c26011ba0cf5ca
HKristof25 <21.hajdu.kristof@mechwart.com>
2026-01-20 08:29:03 +0100

    home  progress

--- borkatalogus/src/pages/Home.tsx
+++ borkatalogus/src/pages/Home.tsx
@@ -3,14 +3,21 @@ import styles from "../Home.module.css"
 const Home = () => {
   return (
     <div className={styles.HomeMain}>
-      <div className={styles.TextContainer}>
-        <h1>Quality and Quantity at one place!</h1>
-        <h3>A a wide variety of wine from all around the world!</h3>
-        <h3>Trusted and used by thousands of users everyday.</h3>
-        <h1>⭐⭐⭐⭐⭐</h1>
-        <h3>Worldwide shipping in everyday of the year for affordable price.</h3>
-        <h2>Order Now</h2>
-      </div>  
+
+      <div className={styles.HomeFirst}>
+        <div className={styles.TextContainer}>
+          <h1>Quality and Quantity at one place!</h1>
+          <h3>A a wide variety of wine from all around the world!</h3>
+          <h3>Trusted and used by thousands of users everyday.</h3>
+          <h1 className={styles.Stars}>⭐⭐⭐⭐⭐</h1>
+          <h3>Worldwide shipping in everyday of the year for affordable price.</h3>
+          <h2>Order Now 🛒</h2>
+        </div>  
+      </div>
+      <div className={styles.HomeSecond}>
+        
+      </div>
+
     </div>
   )
 }
