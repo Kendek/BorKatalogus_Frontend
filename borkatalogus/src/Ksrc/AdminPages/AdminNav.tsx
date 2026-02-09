5e260008da12b2eb90e4002eeed768a93ffa7eab
HKristof25 <21.hajdu.kristof@mechwart.com>
2026-02-09 10:03:32 +0100

    Admin Navbar done

--- borkatalogus/src/Ksrc/AdminPages/AdminNav.tsx
+++ borkatalogus/src/Ksrc/AdminPages/AdminNav.tsx
@@ -4,10 +4,10 @@ import styles from './Admin.module.css'
 const AdminNav = () => {
   return (
     <div  className={styles.Options}>
-          <Link to="/adminAccounts">Accounts</Link>
-          <Link to="/adminGrape">Grape</Link>
-          <Link to="/adminWine">Wine</Link>
-          <Link to="/adminWinery">Winery</Link>
+          <Link className={location.pathname === "/adminAccounts" ? styles.Active : styles.Option}  to="/adminAccounts">Accounts</Link>
+          <Link className={location.pathname === "/adminGrape" ? styles.Active : styles.Option} to="/adminGrape">Grape</Link>
+          <Link className={location.pathname === "/adminWine" ? styles.Active : styles.Option} to="/adminWine">Wine</Link>
+          <Link className={location.pathname === "/adminWinery" ? styles.Active : styles.Option} to="/adminWinery">Winery</Link>
         </div>
   )
 }
